cbbd4e25d5d3c4cd1d1f386fdbf4a61a8fd8976b
thuanphatt <zelorntp@gmail.com>
2023-02-12 15:42:57 +0700

    build navbar #1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template lang="">
+<v-app-bar app color="black" flat class="px-12">
+    <v-btn>
+        <v-icon color="blue-darken-4" left class="mr-2">
+            fas fa-signature
+        </v-icon>
+        TPhat
+    </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text @click="scroll('home')" class="text-blue-darken-4">Home</v-btn>
+        <v-btn text @click="scroll('about')" >About</v-btn>
+        <v-btn text @click="scroll('portfolio')" >Portfolio</v-btn>
+        <v-btn text @click="scroll('services')" >Services</v-btn>
+        <v-btn text @click="scroll('page')" >Page</v-btn>
+        <v-btn text @click="scroll('blog')" >Blog</v-btn>
+        <v-btn text @click="scroll('contact')" >Contact</v-btn>
+
+</v-app-bar>
+</template>
+
+<script>
+export default {
+    methods: {
+        scroll(refName) {
+            const element = document.getElementById(refName)
+            element.scrollIntoView({ behavior: "smooth" })
+        }
+    }
+};
+</script>
+
+<style lang=""></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,76 @@
 <template>
-  <HelloWorld />
+  <v-app id="home">
+    <Navbar />
+    <v-container fluid>
+      <div class="head">
+        <v-row>
+          <v-col cols="5">
+            <div style="position: relative;" class="mt-16">
+              <h1 class="text-grey">Hello,</h1>
+              <h1 class="text-grey">I'm Thuan Phat</h1>
+              <span class="text-grey">Web Designer & Developer</span>
+              <v-btn tile class="text-blue-darken-4 mt-8" variant="outlined">
+                Contact me
+              </v-btn>
+
+            </div>
+          </v-col>
+          <v-col cols="7"></v-col>
+        </v-row>
+      </div>
+    </v-container>
+
+  </v-app>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import Navbar from "../components/NavBar.vue";
 
 export default defineComponent({
-  name: 'HomeView',
+  name: "HomeView",
 
   components: {
-    HelloWorld,
+    Navbar,
   },
 });
 </script>
+<style scoped>
+.v-container {
+  padding: 16px 0;
+}
+
+.head {
+  position: relative;
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width: 100%;
+  color: white;
+}
+
+.head::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, 6deg);
+}
+
+.head::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, -6deg);
+}
+</style>
