c148d1a3edc35db46f0fe77a08071120ea9a27d5
thuanphatt <zelorntp@gmail.com>
2023-02-12 13:28:16 +0700

    setup env & defen

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,18 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@mdi/font/css/materialdesignicons.css";
+import { aliases, fa } from "vuetify/iconsets/fa";
+import "vuetify/styles";
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+});
