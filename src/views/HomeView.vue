c268bc4975166cca7dbedbb1bb50a036233effde
thuanphatt <zelorntp@gmail.com>
2023-02-13 16:37:22 +0700

    build content for website #1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,6 +25,40 @@
           </v-col>
         </v-row>
       </div>
+      <v-col cols="12" class="mt-16" id="about">
+        <div>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <div class="egg">
+                <v-img src="i2.png" max-height="300"></v-img>
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <h5 class="mt-16">About me</h5>
+              <div style="width:120px">
+                <v-slider v-model="slider2" color="blue-darken-4" track-color=""></v-slider>
+              </div>
+              <h4 class="mt-n4">I am a Creative Web Design</h4>
+              <h4>And Developer</h4>
+              <p class="text-grey">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem nobis neque quasi
+                commodi tenetur praesentium? Assumenda consectetur repellendus pariatur veniam nisi. Fugiat consequuntur
+                laudantium dolorum obcaecati deserunt ipsum velit ab!</p>
+              <br />
+              <p class="text-grey">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem nobis neque quasi
+                commodi tenetur praesentium? Assumenda consectetur repellendus pariatur veniam nisi. Fugiat consequuntur
+                laudantium dolorum obcaecati deserunt ipsum velit ab!</p>
+              <v-btn tile dark color="blue-darken-4" class="mt-4">Download Resume</v-btn>
+            </v-col>
+          </v-row>
+        </div>
+
+      </v-col>
+      <div class="text-center mt-4">
+        <h2>What We Do</h2>
+        <div style="width: 120px; margin: 0 auto;">
+          <v-slider v-model="slider2" color="blue-darken-4" track-color=""></v-slider>
+        </div>
+      </div>
     </v-container>
 
   </v-app>
@@ -38,6 +72,31 @@ import Navbar from "../components/NavBar.vue";
 
 export default defineComponent({
   name: "HomeView",
+  setup() {
+    return {
+      slider2: 50,
+      items: [
+        {
+          img: "i3.jpg"
+        },
+        {
+          img: "i4.jpg"
+        },
+        {
+          img: "i5.jpg"
+        },
+        {
+          img: "i6.jpg"
+        },
+        {
+          img: "i9.jpg"
+        },
+        {
+          img: "i8.jpg"
+        },
+      ]
+    }
+  },
 
   components: {
     Navbar,
@@ -79,5 +138,15 @@ export default defineComponent({
   width: 50%;
   background: black;
   transform: skew(0deg, -6deg);
+}
+
+.egg {
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background-color: #0D47A1;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
 }
 </style>
